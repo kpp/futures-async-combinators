44ba8e9ce86039885146eb333acf8503adb10e18
Roman Proskuryakov <humbug@deeptown.org>
2019-02-23 12:46:14 +0300

    Add examples/stream.rs

---
+++ examples/stream.rs
@@ -0,0 +1,15 @@
+#![feature(futures_api, async_await, await_macro)]
+
+use futures_async_combinators::stream::*;
+use futures::{stream, executor};
+
+fn main() {
+    let stream = stream::iter(1..=3);
+    let stream = map(stream, |x| x + 1);
+    let stream = map(stream, |x| x * 2);
+
+    let collect_future = collect(stream);
+    let collection : Vec<_> = executor::block_on(collect_future);
+
+    assert_eq!(vec![4, 6, 8], collection);
+}
