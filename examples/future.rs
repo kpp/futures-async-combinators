5f7e5b3e731733507b1557e25d5da02c7e299c63
Roman Proskuryakov <humbug@deeptown.org>
2019-02-24 01:41:25 +0300

    Replace futures::future::ready with our implementation in examples

--- examples/future.rs
+++ examples/future.rs
@@ -1,12 +1,12 @@
 #![feature(futures_api, async_await, await_macro)]
 
 use futures_async_combinators::future::*;
-use futures::{future, executor};
+use futures::executor;
 
 fn main() {
     executor::block_on(async {
-        let future = future::ready(Ok::<i32, i32>(1));
-        let future = and_then(future, |x| future::ready(Ok::<i32, i32>(x + 3)));
+        let future = ready(Ok::<i32, i32>(1));
+        let future = and_then(future, |x| ready(Ok::<i32, i32>(x + 3)));
         let future = inspect(future, |x| { dbg!(x); });
         assert_eq!(await!(future), Ok(4));
     });
