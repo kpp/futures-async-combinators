a95897c41904105e2107f587926f76c208b3111c
Roman Proskuryakov <humbug@deeptown.org>
2020-03-29 02:04:10 +0300

    Fix benches: actually poll futures in benches

--- benches/future.rs
+++ benches/future.rs
@@ -2,22 +2,24 @@ use criterion::*;
 use futures::executor;
 
 fn bench_ready(c: &mut Criterion) {
-    executor::block_on(async {
-        let mut group = c.benchmark_group("future::ready");
+    let mut group = c.benchmark_group("future::ready");
 
-        group.bench_function("futures", |b| {
-            b.iter(move || async {
-                black_box(futures::future::ready(42)).await
+    group.bench_function("futures", |b| {
+        b.iter(|| {
+            executor::block_on(async {
+                black_box(futures::future::ready(42).await);
             })
-        });
-        group.bench_function("async_combinators", |b| {
-            b.iter(move || async {
-                black_box(futures_async_combinators::future::ready(42)).await
-            })
-        });
-
-        group.finish();
+        })
     });
+    group.bench_function("async_combinators", |b| {
+        b.iter(|| {
+            executor::block_on(async {
+                black_box(futures_async_combinators::future::ready(42).await);
+            })
+        })
+    });
+
+    group.finish();
 }
 
 fn bench_poll_fn(c: &mut Criterion) {
@@ -27,47 +29,51 @@ fn bench_poll_fn(c: &mut Criterion) {
         Poll::Ready(42)
     }
 
-    executor::block_on(async {
-        let mut group = c.benchmark_group("future::poll_fn");
+    let mut group = c.benchmark_group("future::poll_fn");
 
-        group.bench_function("futures", |b| {
-            b.iter(move || async {
-                black_box(futures::future::poll_fn(ret_42)).await
+    group.bench_function("futures", |b| {
+        b.iter(|| {
+            executor::block_on(async {
+                black_box(futures::future::poll_fn(ret_42).await);
             })
-        });
-        group.bench_function("async_combinators", |b| {
-            b.iter(move || async {
-                black_box(futures_async_combinators::future::poll_fn(ret_42)).await
-            })
-        });
-
-        group.finish();
+        })
     });
+    group.bench_function("async_combinators", |b| {
+        b.iter(|| {
+            executor::block_on(async {
+                black_box(futures_async_combinators::future::poll_fn(ret_42)).await;
+            })
+        })
+    });
+
+    group.finish();
 }
 
 fn bench_map(c: &mut Criterion) {
-    executor::block_on(async {
-        let mut group = c.benchmark_group("future::map");
+    let mut group = c.benchmark_group("future::map");
 
-        group.bench_function("futures", |b| {
-            b.iter(move || async {
+    group.bench_function("futures", |b| {
+        b.iter(|| {
+            executor::block_on(async {
                 use futures::future::*;
                 let fut = ready(40);
                 let fut = fut.map(|x| x + 2);
-                black_box(fut).await
+                black_box(fut.await);
             })
-        });
-        group.bench_function("async_combinators", |b| {
-            b.iter(move || async {
+        })
+    });
+    group.bench_function("async_combinators", |b| {
+        b.iter(|| {
+            executor::block_on(async {
                 use futures_async_combinators::future::*;
                 let fut = ready(40);
                 let fut = map(fut, |x| x + 2);
-                black_box(fut).await
+                black_box(fut.await);
             })
-        });
-
-        group.finish();
+        })
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, bench_ready, bench_poll_fn, bench_map);
