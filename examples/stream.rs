04718c41b5a2c2ace8f6856d80c6206eed0fcb95
Roman Proskuryakov <r.proskuryakoff@gmail.com>
2020-12-05 22:28:34 +0300

    Use code style in example/stream as in future's

--- examples/stream.rs
+++ examples/stream.rs
@@ -1,5 +1,3 @@
-#![type_length_limit="2111998"]
-
 use futures::executor;
 use futures_async_combinators::stream::*;
 
@@ -7,6 +5,9 @@ fn main() {
     let stream = iter(1..=3);
     let stream = map(stream, |x| x + 1);
     let stream = map(stream, |x| x * 2);
+    let stream = inspect(stream, |x| {
+        dbg!(x);
+    });
 
     let collect_future = collect(stream);
     let collection: Vec<_> = executor::block_on(collect_future);
